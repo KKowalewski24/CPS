cf4180319d69ca220e04eab07a20f05fe387e659
Kamil Kowalewski <szamil128@onet.pl>
2020-03-05 18:56:36 +0100

    Improve FxmlStageSetup

--- src/main/java/pl/jkkk/cps/view/util/FxmlStageSetup.java
+++ src/main/java/pl/jkkk/cps/view/util/FxmlStageSetup.java
@@ -3,8 +3,6 @@ package pl.jkkk.cps.view.util;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,7 +11,6 @@ public class FxmlStageSetup {
 
     /*------------------------ FIELDS REGION ------------------------*/
     private static Stage applicationStage;
-    private static PopOutWindow popOutWindow = new PopOutWindow();
 
     /*------------------------ METHODS REGION ------------------------*/
     private FxmlStageSetup() {
@@ -27,56 +24,43 @@ public class FxmlStageSetup {
         FxmlStageSetup.applicationStage = applicationStage;
     }
 
+    /**
+     * Method load fxml file.
+     */
     private static Parent loadFxml(String fxml) throws IOException {
         return new FXMLLoader(FxmlStageSetup.class.getResource(fxml)).load();
     }
 
-    public static void buildStage(String filePath) throws IOException {
+    /**
+     * Method load stage from scratch and set `applicationStage` - use on startup of application
+     * stage is passed from start method from Main class.
+     */
+    public static void buildStage(Stage stage, String filePath, String title) throws IOException {
+        setApplicationStage(stage);
         applicationStage.setScene(new Scene(loadFxml(filePath)));
+        applicationStage.setTitle(title);
         applicationStage.sizeToScene();
         applicationStage.show();
     }
 
-    public static void buildStage(Stage stage, String filePath, String title) throws IOException {
-        setApplicationStage(stage);
-        stage.setScene(new Scene(loadFxml(filePath)));
-        stage.setTitle(title);
-        stage.sizeToScene();
-        stage.show();
+    /**
+     * Method load new stage and set `applicationStage` to a new one but leave the previous one
+     * open.
+     */
+    public static void loadStage(String filePath, String title) throws IOException {
+        setApplicationStage(new Stage());
+        applicationStage.setScene(new Scene(loadFxml(filePath)));
+        applicationStage.setTitle(title);
+        applicationStage.sizeToScene();
+        applicationStage.show();
     }
 
-    public static void closeStageByButton(Button button) {
-        Stage currentStage = (Stage) button.getScene().getWindow();
-        currentStage.close();
-    }
-
-    public static void reloadStage(Button button, String fxmlPath,
-                                   String fxmlStylePath, String title) {
-        closeStageByButton(button);
-        Stage mainStage = loadFxmlStage(fxmlPath, fxmlStylePath, title);
+    /**
+     * Method close previous stage and load new stage and set `applicationStage` to a new one.
+     */
+    public static void reloadStage(String filePath, String title) throws IOException {
         applicationStage.close();
-        applicationStage = mainStage;
-    }
-
-    //TODO
-    public static Stage loadFxmlStage(String fxmlPath, String fxmlStylePath, String title) {
-        try {
-            Scene scene = new Scene(FXMLLoader.load(FxmlStageSetup.class.getResource(fxmlPath)));
-            scene.getStylesheets().add(FxmlStageSetup.class.getResource(fxmlStylePath).toExternalForm());
-
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.setTitle(title);
-            stage.setResizable(false);
-            stage.show();
-
-            return stage;
-        } catch (IOException e) {
-            popOutWindow.messageBox("Stage Loading Error",
-                    "Cannot Properly Load Main Stage", Alert.AlertType.ERROR);
-        }
-
-        return null;
+        loadStage(filePath, title);
     }
 }
     
