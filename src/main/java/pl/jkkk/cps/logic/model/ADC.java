657453037227b07f53c9f444df62a6382fbb7d87
Jan Karwowski <karwojan@tlen.pl>
2020-03-21 20:14:53 +0100

    Implement ADC sampling

--- src/main/java/pl/jkkk/cps/logic/model/ADC.java
+++ src/main/java/pl/jkkk/cps/logic/model/ADC.java
@@ -1,20 +1,27 @@
 package pl.jkkk.cps.logic.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import pl.jkkk.cps.logic.model.signal.ContinuousSignal;
 
 public class ADC {
 
-    public List<Data> sampling(ContinuousSignal signal, int sampleRate){
+    public List<Double> sampling(ContinuousSignal signal, int sampleRate){
+        int numberOfSamples = (int)(signal.getRangeLength() * sampleRate);
+        List<Double> data = new ArrayList<>();
+        Double step = signal.getRangeLength() / (numberOfSamples - 1);
+        for(int i = 0; i < numberOfSamples; i++){
+            data.add(signal.value(i * step + signal.getRangeStart()));
+        }
+        return data;
+    }
+
+    public List<Double> roundingQuantization(List<Double> samples, int numberOfLevels){
         return null;
     }
 
-    public List<Data> roundingQuantization(List<Data> samples, int numberOfLevels){
-        return null;
-    }
-
-    public List<Data> truncatingQuantization(List<Data> samples, int numberOfLevels){
+    public List<Double> truncatingQuantization(List<Double> samples, int numberOfLevels){
         return null;
     } 
 }
