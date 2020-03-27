f2c35f7495b0f9f297b5182536a0c5ac38869634
Kamil Kowalewski <szamil128@onet.pl>
2020-03-27 09:52:10 +0100

    Fix formatting

--- src/main/java/pl/jkkk/cps/logic/model/ADC.java
+++ src/main/java/pl/jkkk/cps/logic/model/ADC.java
@@ -1,10 +1,10 @@
 package pl.jkkk.cps.logic.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import pl.jkkk.cps.logic.model.signal.ContinuousSignal;
 import pl.jkkk.cps.logic.model.signal.Signal;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ADC {
 
@@ -25,10 +25,10 @@ public class ADC {
         List<Data> data = new ArrayList<>();
         signal.getData().forEach(sample -> {
             int index = (int) Math.round(
-                        (sample.getY() - levels.get(0)) /
-                        (levels.get(levels.size() - 1) - levels.get(0)) *
-                        (levels.size() - 1)
-                    );
+                    (sample.getY() - levels.get(0))
+                            / (levels.get(levels.size() - 1) - levels.get(0))
+                            * (levels.size() - 1)
+            );
             data.add(new Data(sample.getX(), levels.get(index)));
         });
         return createSignalBasingOnExistingData(data);
@@ -39,20 +39,20 @@ public class ADC {
         List<Data> data = new ArrayList<>();
         signal.getData().forEach(sample -> {
             int index = (int) Math.floor(
-                        (sample.getY() - levels.get(0)) /
-                        (levels.get(levels.size() - 1) - levels.get(0)) *
-                        (levels.size() - 1)
-                    );
+                    (sample.getY() - levels.get(0))
+                            / (levels.get(levels.size() - 1) - levels.get(0))
+                            * (levels.size() - 1)
+            );
             data.add(new Data(sample.getX(), levels.get(index)));
         });
         return createSignalBasingOnExistingData(data);
-    } 
+    }
 
-    private Signal createSignalBasingOnExistingData(List<Data> existingData){
-        return new Signal(existingData.size()){
+    private Signal createSignalBasingOnExistingData(List<Data> existingData) {
+        return new Signal(existingData.size()) {
             @Override
-            public void generate(){
-                for(int i = 0; i < existingData.size(); i++){
+            public void generate() {
+                for (int i = 0; i < existingData.size(); i++) {
                     this.data[i] = existingData.get(i);
                 }
             }
@@ -62,9 +62,9 @@ public class ADC {
     private List<Double> calculateLevels(Signal signal, int numberOfLevels) {
         List<Double> levels = new ArrayList<>();
         double min = signal.getData().stream()
-            .mapToDouble(sample -> sample.getY()).min().getAsDouble();
+                .mapToDouble(sample -> sample.getY()).min().getAsDouble();
         double max = signal.getData().stream()
-            .mapToDouble(sample -> sample.getY()).max().getAsDouble();
+                .mapToDouble(sample -> sample.getY()).max().getAsDouble();
         double step = (max - min) / (numberOfLevels - 1);
         for (int i = 0; i < numberOfLevels; i++) {
             levels.add(step * i + min);
--- src/main/java/pl/jkkk/cps/logic/model/DAC.java
+++ src/main/java/pl/jkkk/cps/logic/model/DAC.java
@@ -1,33 +1,34 @@
 package pl.jkkk.cps.logic.model;
 
-import pl.jkkk.cps.logic.model.Data;
 import pl.jkkk.cps.logic.model.signal.ContinuousSignal;
 import pl.jkkk.cps.logic.model.signal.DiscreteSignal;
 
 public class DAC {
-    
-    public ContinuousSignal zeroOrderHold(DiscreteSignal signal){
-        return new ContinuousSignal(signal.getRangeStart(), signal.getRangeLength()){
+
+    public ContinuousSignal zeroOrderHold(DiscreteSignal signal) {
+        return new ContinuousSignal(signal.getRangeStart(), signal.getRangeLength()) {
             @Override
-            public double value(double t){
-                int index = (int) Math.floor((t - signal.getRangeStart()) / signal.getRangeLength() * 
-                        (signal.getData().size() - 1));
+            public double value(double t) {
+                int index =
+                        (int) Math.floor((t - signal.getRangeStart()) / signal.getRangeLength()
+                                * (signal.getData().size() - 1));
                 return signal.getData().get(index).getY();
             }
         };
     }
 
-    public ContinuousSignal firstOrderHold(DiscreteSignal signal){
-        return new ContinuousSignal(signal.getRangeStart(), signal.getRangeLength()){
+    public ContinuousSignal firstOrderHold(DiscreteSignal signal) {
+        return new ContinuousSignal(signal.getRangeStart(), signal.getRangeLength()) {
             @Override
-            public double value(double t){
-                int index = (int) Math.floor((t - signal.getRangeStart()) / signal.getRangeLength() * 
-                        (signal.getData().size() - 1));
+            public double value(double t) {
+                int index =
+                        (int) Math.floor((t - signal.getRangeStart()) / signal.getRangeLength()
+                                * (signal.getData().size() - 1));
                 Data A = signal.getData().get(index);
-                if(index < signal.getData().size() - 1){
+                if (index < signal.getData().size() - 1) {
                     Data B = signal.getData().get(index + 1);
                     return (t - A.getX()) / (B.getX() - A.getX()) * (B.getY() - A.getY()) + A.getY();
-                }else{
+                } else {
                     return A.getY();
                 }
             }
