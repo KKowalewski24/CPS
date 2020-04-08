112473b992c09d0a138e41327902e5172865e3f8
Jan Karwowski <jan.karwowski@makolab.com>
2020-04-09 01:29:36 +0200

    Continue implementing text interface

--- src/main/java/pl/jkkk/cps/textinput/MainText.java
+++ src/main/java/pl/jkkk/cps/textinput/MainText.java
@@ -1,7 +1,20 @@
 package pl.jkkk.cps.textinput;
 
+import pl.jkkk.cps.logic.model.signal.Signal;
+import pl.jkkk.cps.logic.model.signal.UnitJumpSignal;
+
 public class MainText {
-    public void main(String[] args){
-        System.out.println("Hello world");
+    public void main(String[] args) {
+        Operation operation = Operation.fromString(args[0]);
+        switch (operation) {
+        case GENERATE:
+            Signal signal = null;
+            SignalType signalType = SignalType.fromString(args[1]);
+            switch (signalType) {
+            case UNIT_JUMP:
+                signal = new UnitJumpSignal(Double.parseDouble(args[2]), Double.parseDouble(args[3]),
+                        Double.parseDouble(args[4]), Double.parseDouble(args[5]));
+            }
+        }
     }
 }
---
+++ src/main/java/pl/jkkk/cps/textinput/SignalType.java
@@ -0,0 +1,28 @@
+package pl.jkkk.cps.textinput;
+
+import java.util.Arrays;
+
+public enum SignalType {
+    UNIFORM_NOISE("uni_noise"),
+    GAUSSIAN_NOISE("gauss_noise"),
+    SINUSOIDAL_SIGNAL("sin"),
+    SINUSOIDAL_RECTIFIED_ONE_HALF_SIGNAL("sin_one_half"),
+    SINUSOIDAL_RECTIFIED_IN_TWO_HALVES("sin_two_half"),
+    RECTANGULAR_SIGNAL("rect"),
+    SYMMETRICAL_RECTANGULAR_SIGNAL("rect_symm"),
+    TRIANGULAR_SIGNAL("triang"),
+    UNIT_JUMP("unit_jump"),
+    UNIT_IMPULSE("unit_impulse"),
+    IMPULSE_NOISE("impulse_noise");
+
+    private final String abbreviation;
+
+    SignalType(final String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    public static SignalType fromString(final String text) {
+        return Arrays.asList(SignalType.values()).stream().filter(signal -> signal.abbreviation.equals(text))
+                .findFirst().orElseThrow(IllegalArgumentException::new);
+    }
+}
