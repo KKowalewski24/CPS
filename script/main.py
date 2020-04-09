ac70729bbf4d541c96e0dc1923d15e154ccc5146
Kamil Kowalewski <szamil128@onet.pl>
2020-04-09 15:50:24 +0200

    Add python script

--- script/main.py
+++ script/main.py
@@ -1,18 +1,39 @@
 import subprocess
 
+'''
+
+How to use
+Copy generated jar to directory where this python script is located, 
+the call run_jar() function, 
+if empty array is passed jar is being called with 0 args
+if you want to pass args just place them in array as a string
+
+'''
+
 JAR_NAME = "cps-0.0.1-jar-with-dependencies.jar"
 
 
 # ----------------------------------------------------------------------------- #
-def run_jar():
-    subprocess.call(
-        ["java", "-jar", JAR_NAME]
-    )
+def run_jar(args):
+    command = ["java", "-jar", JAR_NAME]
+    for it in args:
+        command.append(it)
+
+    subprocess.call(command)
+
+
+def series_1():
+    run_jar(["generate", "uni_noise", "1", "5", "1"])
+
+
+def series_2():
+    run_jar([])
 
 
 # ----------------------------------------------------------------------------- #
 def main():
-    # run_jar()
+    series_1()
+    # series_2()
 
     print("------------------------------------------------------------------------")
     print("FINISHED SUCCESSFULLY")
