b7c3d84940c7d1fb909523c981920010cfc38f9e
Jan Karwowski <karwojan@tlen.pl>
2020-04-18 17:16:06 +0200

    Implement correlation signal

--- src/main/java/pl/jkkk/cps/logic/model/signal/ConvolutionSignal.java
+++ src/main/java/pl/jkkk/cps/logic/model/signal/ConvolutionSignal.java
@@ -7,7 +7,7 @@ public class ConvolutionSignal extends DiscreteSignal {
 
     public ConvolutionSignal(DiscreteSignal h, DiscreteSignal x){
         super(h.getRangeStart(), 
-                (h.getNumberOfSamples() + x.getNumberOfSamples() - 1), 
+                h.getNumberOfSamples() + x.getNumberOfSamples() - 1, 
                 h.getSampleRate());
         this.h = h;
         this.x = x;
@@ -17,8 +17,8 @@ public class ConvolutionSignal extends DiscreteSignal {
     public double value(int i){
         double sum = 0.0;
         int startK = Math.max(0, i - x.getNumberOfSamples() + 1);
-        int endX = Math.min(h.getNumberOfSamples(), i + 1);
-        for (int k = startK; k < endX; k++) {
+        int endK = Math.min(h.getNumberOfSamples(), i + 1);
+        for (int k = startK; k < endK; k++) {
             sum += h.value(k) * x.value(i - k);
         }
         
