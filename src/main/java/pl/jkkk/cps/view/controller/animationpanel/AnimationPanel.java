1e2e05a78d6ded7a7778dde2e2847d5a788776d0
Kamil Kowalewski <szamil128@onet.pl>
2020-04-18 22:15:24 +0200

    Add new window + code cleanup

---
+++ src/main/java/pl/jkkk/cps/view/controller/animationpanel/AnimationPanel.java
@@ -0,0 +1,18 @@
+package pl.jkkk.cps.view.controller.animationpanel;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AnimationPanel implements Initializable {
+
+    /*------------------------ FIELDS REGION ------------------------*/
+
+    /*------------------------ METHODS REGION ------------------------*/
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
+    
