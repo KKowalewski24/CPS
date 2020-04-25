8636e6f4d9fe6a6c1430ebcf0fa0649537a8c0ff
Jan Karwowski <karwojan@tlen.pl>
2020-04-25 14:42:33 +0200

    Make Operation serializable

--- src/main/java/pl/jkkk/cps/logic/model/Operation.java
+++ src/main/java/pl/jkkk/cps/logic/model/Operation.java
@@ -1,6 +1,8 @@
 package pl.jkkk.cps.logic.model;
 
+import java.io.Serializable;
+
 @FunctionalInterface
-public interface Operation {
+public interface Operation extends Serializable{
     double operation(double a, double b);
 }
